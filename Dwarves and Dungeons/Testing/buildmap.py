d5aeb9f63e57f6ce81e019f5d793948c48573dce
Terokahi <kilianguntermann@gmx.de>
2024-10-28 16:41:30 +0100

    Done for the day

---
+++ Dwarves and Dungeons/Testing/buildmap.py
@@ -0,0 +1,20 @@
+# ore Generation (Separate File)
+import termcolor as colored
+
+iron = ["iron", "red", 0]
+silver = ["silver", "Green", 1]
+tin = ["tin", "grey", 2]
+gold = ["gold", "yellow", 3]
+copper = ["copper", "orange", 4]
+bronze = ["bronze", "brown", 5]
+
+def main():
+    """Main function."""
+    
+
+# Biom Generation (Separate File)
+# Special Rooms (Separate File)
+# Mineshafts (Separate File)
+# empty rooms (Separate File)(Copy roomGen)
+# player (Separate File)
+# mobs (Separate File)
