14e5e5cb70ef953b19026194e097e2b8438534fa
Terokashi <kilianguntermann@gmx.de>
2024-11-21 14:22:11 +0100

    Reworked everything
    installed dotnet.
    Everything broke.
    Pulled from Ffaeble WG repo.
    edited for own use.

---
+++ scripts/MapGen.cs
@@ -0,0 +1,107 @@
+using Godot;
+using System;
+
+public partial class MapGen : Node2D
+{
+	/// The layers of the map
+	/// @wall > The layer that is used to block the player's movement
+	/// @floor > The layer that is used as the visible floor of the map
+	/// @fluid > The layer that is used for fluids such as lava and water
+	private TileMapLayer wall, floor, fluid;
+	
+	/// Sets the wall layer of the map
+	/// <param name="node">The wall layer of the map
+	public void setWall()
+	{
+		wall = GetNode<TileMapLayer>("Wall");
+	}
+	
+	/// Sets the floor layer of the map
+	/// <param name="node">The floor layer of the map
+	public void setFloor()
+	{
+		floor = GetNode<TileMapLayer>("Floor");
+	}
+	
+	/// Sets the fluid layer of the map
+	/// <param name="node">The fluid layer of the map
+	public void setFluid()
+	{
+		fluid = GetNode<TileMapLayer>("Fluid");
+	}
+
+	/// Getter Methods for the Layers
+	public TileMapLayer getWall => wall;
+	public TileMapLayer getFloor => floor;
+	public TileMapLayer getFluid => fluid;
+	
+	/// Called when the node enters the scene tree for the first time
+	public override void _Ready()
+	{
+		setWall();
+		setFloor();
+		/// setFluid();
+
+		// Set Map Size relative to screen Size
+		// Saved in var for future use
+		int width = 1920;
+		int height = 1080;
+		
+		MapInit(new Vector2I(0,0), new Vector2I(width, height));
+	}
+	
+	/// Initializes the map with the given size and wall layer
+	/// <param name="X">The width of the map
+	/// <param name="Y">The height of the map
+	/// <param name="wall">The wall layer of the map
+	
+	/// This function sets the map to a solid stone layer.
+
+	/// <remarks>
+	/// This function is used to initialize the map with a solid stone layer.
+	/// The map is then ready to be used with the room generation algorithm.
+	private void MapInit(Vector2I pos, Vector2I size, type)
+	{	
+		Vector2I SolidStone = new Vector2I(2,1);
+		int TS_ID = 0;
+		wall = getWall;
+		for (int x = pos.X -1; x < size.X; x++)
+		{
+			for (int y = pos.Y -1; y < size.Y; y++)
+			{
+				Vector2I Tilepos = new Vector2I(x,y);
+				wall.SetCell(Tilepos,TS_ID,SolidStone);
+			}
+		}
+	}
+	
+	/*
+	GD.Randomize()	
+	roomAmount = random.RandiRange(1, 400)
+	while roomAmount > 0
+	{
+		setArea(pos, size, TS_ID);
+	} 
+	*/
+	
+	// replaces MapInit() later please
+	private void setArea(Vector2I pos, Vector2I size, [TML_ID, TS_ID])
+	{	
+		if wall.GetCellSourceId(pos) == TS_ID
+		Vector2I SolidStone = new Vector2I(2,1);
+		wall = getFloor;
+		for (int x = pos.X -1; x < size.X; x++)
+		{
+			for (int y = pos.Y -1; y < size.Y; y++)
+			{
+				Vector2I Tilepos = new Vector2I(x,y);
+				wall.SetCell(Tilepos,TS_ID,SolidStone);
+			}
+		}
+	}
+	/// Called every frame. 'delta' is the elapsed time since the previous frame
+	/// <param name="delta">The time elapsed since the previous frame
+	public override void _Process(double delta)
+	{
+	}
+}
