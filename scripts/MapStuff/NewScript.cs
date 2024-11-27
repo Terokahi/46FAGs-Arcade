378daaa31ce1fb82e85b8772a4a7903fd35bacec
Ffaeble <trashfairy@outlook.de>
2024-11-27 17:10:25 +0100

    Overload
    changed  some dicts to enum.
    tried to get better naming sense for dicts and enums.
    added init function for one dictionary
    changed enums back because enums have to be converted to int explicitly and that is ass.
    added layerID param to UpdateTilemapLayers (renamed to be less of a mouthful)
    and removed the z loop inside since it is no longer neccessary
    removed unneccessary comments
    
    changed init to overloading UpdateTileMapLayers
    added a bunch of other questionable useful overload functions

--- scripts/MapStuff/NewScript.cs
+++ scripts/MapStuff/NewScript.cs
@@ -38,97 +38,125 @@ namespace MapGen
 			{ 15, new Vector2I(2, 1) }
 		};
 
-		private Dictionary<int,TileMapLayer> LayerFromID;
-		private Dictionary<string,int> IDFromLayerName;
-		private Dictionary<string,int> TS_ID;
+		private Dictionary<int,TileMapLayer> LayerRegistry;
+		private static Dictionary<string, int> getLayer_ID = new Dictionary<string, int>(){
+			{"DecOres", 0},
+			{"Collision", 1},
+			{"Walkable", 2},
+			{"Diveable", 3}
+		};
+		private static Dictionary<string, int> getTS_ID = new Dictionary<string, int>(){
+			{"none", -1},
+			{"stone", 0},
+			{"dirt", 1},
+			{"water", 37}
+		};
 
+		int map_width = 20;
+		int map_height = 20;
+		int[,,] map;
 		public override void _Ready()
 		{
-			LayerFromID = new(){
+			registerLayers();
+			initMapArray();
+			UpdateTileMapLayers();
+		}
+		private void registerLayers(){
+			LayerRegistry = new(){
 				{0, GetNode<TileMapLayer>("DecOres")},
 				{1, GetNode<TileMapLayer>("Collision")},
 				{2, GetNode<TileMapLayer>("Walkable")},
 				{3, GetNode<TileMapLayer>("Diveable")}
 			};
-
-			IDFromLayerName = new(){
-				{"DecOres", 0},
-				{"Collision", 1},
-				{"Walkable", 2},
-				{"Diveable", 3}
-			};
-			TS_ID = new(){
-				{"void", -1},
-				{"stone", 0},
-				{"dirt", 1},
-				{"water",37}
-			};
-
-			int x = 20;
-			int y = 20;
-			int z = LayerFromID.Count;
-			int[,,] map = new int[x, y, z];
-			var indices = Enumerable.Range(0, x * y * z).Select(i => new { 
-				x = i % x,
-				y = i % (x * y) / x,
-				z = i / (x * y) });
-			foreach (var idx in indices){
-				map[idx.x, idx.y, IDFromLayerName["DecOres"]] = TS_ID["void"];
-				map[idx.x, idx.y, IDFromLayerName["Collision"]] = TS_ID["stone"];
-				map[idx.x, idx.y, IDFromLayerName["Walkable"]] = TS_ID["dirt"];
-				map[idx.x, idx.y, IDFromLayerName["Diveable"]] = TS_ID["water"];
-			}
-
-			UpdateRectInLayersFromArray(map, new Rect2I(0, 0, x, y));
-
-			/* /// loop to find tileMapLayers to use later maybe: 
-				Node[] nodes = GetNode("../..").GetChildren().ToArray();
-				foreach (Godot.Node node in nodes){
-					if (node is TileMapLayer layer){}
-				}
-			*/
 		}
 
-		/// Updates the layers from an array.
-		/// <param name="map">The array to update the layers from.</param>
-		/// <param name="pos">The position of the array in the tilemap.</param>
-		private void UpdateRectInLayersFromArray(int[,,] map, Rect2I pos)
+		private void initMapArray(){
+			map = new int[map_width, map_height, getLayer_ID.Count];
+			var indices = Enumerable.Range(0, map_width * map_height * getLayer_ID.Count).Select(i => new { 
+				x = i % map_width,
+				y = (i % (map_width * map_height)) / map_width,
+				z = i / (map_width * map_height) 
+			});
+			foreach (var idx in indices){
+				map[idx.x, idx.y, getLayer_ID["DecOres"]] = getTS_ID["none"];
+				map[idx.x, idx.y, getLayer_ID["Collision"]] = getTS_ID["stone"];
+				map[idx.x, idx.y, getLayer_ID["Walkable"]] = getTS_ID["dirt"];
+				map[idx.x, idx.y, getLayer_ID["Diveable"]] = getTS_ID["water"];
+			}
+		}
+
+		/// Updates all the layers in the LayerRegistry.
+		/// This function is used to update all the layers in the LayerRegistry.
+		/// It loops through each layer in the LayerRegistry and calls the UpdateTilemapLayers.
+		private void UpdateTileMapLayers(){
+			foreach (var layer in LayerRegistry){
+				UpdateTileMapLayers(new Rect2I(0,0,map_width,map_height), layer.Key);
+			}
+		}
+
+		/// Updates a single cell on the given layer.
+		/// <param name="pos">The position of the cell to update.
+		/// <param name="layerID">The id of the layer to update.
+		private void UpdateTileMapLayers(Vector2I pos, int layerID){
+			UpdateTileMapLayers(new Rect2I(pos,pos), layerID);
+		}
+		
+		/// Updates all layers at a specific position.
+		/// This function iterates over each layer in the LayerRegistry and updates the tilemap 
+		/// layers at the specified position.
+		/// <param name="pos">The position of the cell to update across all layers.
+		private void UpdateTileMapLayers(Vector2I pos){
+			foreach (var layer in LayerRegistry){
+				UpdateTileMapLayers(new Rect2I(pos,pos), layer.Key);
+			}
+		}
+		/// Updates a given Rect2I portion on all layers.
+		/// This function iterates over each layer in the LayerRegistry and updates the tilemap 
+		/// layers within the given Rect2I.
+		/// <param name="pos">The Rect2I that contains the cells to update across all layers.
+		private void UpdateTileMapLayers(Rect2I pos){
+			foreach (var layer in LayerRegistry){
+				UpdateTileMapLayers(pos, layer.Key);
+			}
+		}
+
+		/// Updates a given Rect2I portion on the given Layer.
+		/// <param name="pos">The position of the Rect2I.
+		/// <param name="layerID">The id of the layer to update.
+		private void UpdateTileMapLayers(Rect2I pos, int layerID)
 		{
 			//Loop through each cell in the array
 			for (int x = pos.Position.X; x < pos.Size.X + 1; x++)
 			{
 				for (int y = pos.Position.Y; y < pos.Size.Y + 1; y++)
 				{
-					for (int z = 0; z < map.GetLength(2); z++)
-					{
-						//Get the current cell from the array
-						int br = map[x, y, z];
+					//Get the current cell from the array
+					int br = map[x, y, layerID];
 
-						//Get the cells on the top left and right and bottom left and right (-current cell) 
-						//a 2x2 to work for dual grid
-						int bl,tr,tl;
-						try { bl = map[x - 1, y, z]; } catch (IndexOutOfRangeException) { bl = TS_ID["void"]; }
-						try { tr = map[x, y - 1, z]; } catch (IndexOutOfRangeException) { tr = TS_ID["void"]; }
-						try { tl = map[x - 1, y - 1, z]; } catch (IndexOutOfRangeException) { tl = TS_ID["void"]; }
+					//Get the cells on the top left and right and bottom left and right (-current cell) 
+					//a 2x2 to work for dual grid
+					int bl,tr,tl;
+					try { bl = map[x - 1, y, layerID]; } catch (IndexOutOfRangeException) { bl = getTS_ID["void"]; }
+					try { tr = map[x, y - 1, layerID]; } catch (IndexOutOfRangeException) { tr = getTS_ID["void"]; }
+					try { tl = map[x - 1, y - 1, layerID]; } catch (IndexOutOfRangeException) { tl = getTS_ID["void"]; }
 
-						//Determine the atlas vector based on the neighboring cells
-						int atlasVector = 0;
-						if (br != TS_ID["void"]) atlasVector |= (int)Location.LOW_RIGHT;
-						if (tr != TS_ID["void"]) atlasVector |= (int)Location.TOP_RIGHT;
-						if (tl != TS_ID["void"]) atlasVector |= (int)Location.TOP_LEFT;
-						if (bl != TS_ID["void"]) atlasVector |= (int)Location.LOW_LEFT;
+					//Determine the atlas vector based on the neighboring cells
+					int atlasVector = 0;
+					if (br != getTS_ID["void"]) atlasVector |= (int)Location.LOW_RIGHT;
+					if (tr != getTS_ID["void"]) atlasVector |= (int)Location.TOP_RIGHT;
+					if (tl != getTS_ID["void"]) atlasVector |= (int)Location.TOP_LEFT;
+					if (bl != getTS_ID["void"]) atlasVector |= (int)Location.LOW_LEFT;
 
-						//If the atlas vector is 0, then the current cell is not a collision, so set the walkable layer
-						if (atlasVector != 0){
-							//TODO: understand how this works correctly plx
-							LayerFromID[z].SetCell(new Vector2I(x, y), ((dynamic)tl).Id, NeighborsToAtlas[atlasVector]);
-						}
-						/* stay transparent
+					//If the atlas vector is 0, then the current cell is not a collision, so set the walkable layer
+					if (atlasVector != 0){
+						//TODO: understand how this works correctly plx && run/debug
+						LayerRegistry[layerID].SetCell(new Vector2I(x, y), ((dynamic)tl).Id, NeighborsToAtlas[atlasVector]);
+					}
+					/* stay transparent
 						else{
 							GD.Print("The fuck you doing???");
 						}
-						*/
-					}
+					*/
 				}
 			}
 		}
@@ -138,30 +166,5 @@ namespace MapGen
 		}
 	}
 }
-/*
-	dict.add <LayerFromID, getNode>
-	
-	arr[x,y] = dict.key(0)
-	arr[x,y+1] = 1
-	if arr[x,y] != 0
-	________
-	private var whatIDFromLayerName = new Dictionary<string,TileMapLayer>{
-	{"stone_collision", GetCollision},
-	{"dirt_walkable", GetWalkable},
-	{"stone_walkable", GetWalkable}
-	}
-	private var whatID = new Dictionary<string,int>{
-	{"stone_collision", 0},
-	{"dirt_walkable", 1},
-	{"stone_walkable", 0}
-	}
-	___________
-	string[,] map ="stone_collision"
-	arr[,,] map= LayerFromID
-	___________
-	
-	dict<new Vector2i, (getIDFromLayerName, int)>
-	*/
 
-	
 
